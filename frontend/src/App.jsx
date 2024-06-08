62109773c1e66ca39f62182c99119648980d098d
Aryan Tomar <aryantomar5000@gmail.com>
2024-06-08 09:07:24 +0530

    Configured Tailwind CSS for Styling of Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const App = () => {
     return (
-        <div>App</div>
+        <div className='h-screen w-full bg-zinc-900 flex justify-center items-center'>
+            <p className='h-20 w-[90%] rounded-full bg-blue-500 text-3xl text-white flex justify-center items-center'> 
+                Testing Tailwind CSS Installation 
+            </p>
+        </div>
     )
 }
 
