20d982275a62944202a33e1d0a2f96adc4cd0014
Aryan Tomar <aryantomar5000@gmail.com>
2024-06-09 09:55:39 +0530

    Official Twitter doesn't have a Scrollbar in its Feed
    ---> That's why I also just removed the scrollbar
    from my Twitter Clone's feed

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -5,34 +5,12 @@ import CreatePost from './CreatePost'
 
 
 const Feed = () => {
-  return (
-    <div className='relative w-[600px] xl:min-w-[600px] min-[500px]:border-x-[1px] max-[500px]:mt-[56px] max-[500px]:mb-[56px] border-gray-500 flex flex-col bg-blue-500 overflow-y-auto'>
-        <FeedNavbar />
-        <CreatePost />
-        <CreatePost />
-        <CreatePost />
-        <CreatePost />
-        <CreatePost />
-        <CreatePost />
-        <CreatePost />
-        <CreatePost />
-        <CreatePost />
-        <CreatePost />
-        <CreatePost />
-        <CreatePost />
-        <CreatePost />
-        <CreatePost />
-        <CreatePost />
-        <CreatePost />
-        <CreatePost />
-        <CreatePost />
-        <CreatePost />
-        <CreatePost />
-        <CreatePost />
-        <CreatePost />
-        <CreatePost />
-    </div>
-  )
+	return (
+		<div className='relative scrollbar w-[600px] xl:min-w-[600px] min-[500px]:border-x-[1px] max-[500px]:mt-[56px] max-[500px]:mb-[56px] border-gray-500 flex flex-col bg-blue-500 overflow-y-auto'>
+			<FeedNavbar />
+			
+		</div>
+	)
 }
 
 export default Feed
