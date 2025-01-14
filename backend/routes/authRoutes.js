87511db0e069a85d13c17a4609986401ba027cf7
varunHirequotient <varun.sharma@hirequotient.com>
2025-01-14 11:26:33 +0530

    added changes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,7 +4,7 @@ import { validateNonExistenceOfEmailInDB, sendOTP, validateNonExistenceOfUsernam
 
 
 const router = express.Router(); 
-
+// add comment
 
 router.post('/validate-non-existence-of-email-in-db', validateNonExistenceOfEmailInDB); 
 router.put('/send-otp', sendOTP); 
