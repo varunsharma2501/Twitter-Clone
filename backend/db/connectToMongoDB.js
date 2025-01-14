8bcf125db01a9bed619cdb869b01df1da3630bd6
varunHirequotient <varun.sharma@hirequotient.com>
2025-01-14 11:31:58 +0530

    fix

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const connectToMongoDB = async () => {
     try{
         await mongoose.connect(`${process.env.MONGODB_URI}`); 
-        console.log(`Connected to MongoDB Successfully`); 
+        console.log(`Connected to MongoDB Successfully.`); 
     }
     catch(err){
         console.log(`Error occured while connecting to MongoDB: ${err}`); 
